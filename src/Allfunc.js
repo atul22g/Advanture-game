05f682f449c34f3d6cfcffe21752dc186298a886
Atugatran <atugatran@gmail.com>
2023-02-10 17:55:41 +0530

    initial commit

---
+++ src/Allfunc.js
@@ -0,0 +1,20 @@
+function spin() {
+	let spinning = false
+	return {
+		id: "spin",
+		update() {
+			if (spinning) {
+				this.angle += 1200 * dt()
+				if (this.angle >= 360) {
+					this.angle = 0
+					spinning = false
+				}
+			}
+		},
+		spin() {
+			spinning = true
+		},
+	}
+}
+
+module.exports = {spin}
